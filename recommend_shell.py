16b58c75914e67ba09e669f29e5b94b0462b2025
Daniel Passaro <daniel.passaro@regionalhelpwanted.com>
2015-07-15 14:58:17 -0400

    Initial commit

---
+++ recommend_shell.py
@@ -0,0 +1,17 @@
+"""An environment script to make shell usage easy.
+
+Use::
+
+    python --interactive recommend_shell.py
+
+"""
+# pylint: disable=unused-wildcard-import,wildcard-import,invalid-name
+import os.path
+
+import movielens
+from recommendations import *
+
+from critics import critics
+movies = invert_data(critics)
+movielens = movielens.load_movielens_data(
+    os.path.expanduser('~/Downloads/movielens'))
