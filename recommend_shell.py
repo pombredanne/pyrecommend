a36ba19814249835c639546f45fb9a6244755820
Daniel Passaro <daniel.passaro@regionalhelpwanted.com>
2015-07-24 09:27:05 -0400

    Add some simple fake user data

--- recommend_shell.py
+++ recommend_shell.py
@@ -5,12 +5,34 @@ Use::
     python --interactive recommend_shell.py
 
 """
-# pylint: disable=unused-wildcard-import,wildcard-import,invalid-name
-import os.path
-
+# pylint: disable=invalid-name
+import copy
 import movielens
-from recommendations import *
+import recommendations
 
 from critics import critics
-movies = invert_data(critics)
+movies = recommendations.invert_data(critics)
 movielens = movielens.load_movielens_data('ml-100k')
+
+
+def pad_zero(dicts):
+    """Ensure all dicts have all same keys, inserting zeroes when needed."""
+    all_keys = reduce(lambda a, b: a | b, (d.viewkeys() for d in dicts))
+    for dict_ in dicts:
+        for k in all_keys:
+            dict_.setdefault(k, 0)
+
+
+# A "test sample" of users. Map of usernames to ad IDs they've applied to
+users_src = {
+    'a': (1, 2, 3),
+    'b': (1, 3, 5),
+    'c': (10, 11, 12),
+}
+
+users = {
+    k: {ad_id: 1 for ad_id in v} for k, v in users_src.items()
+}
+
+users_zero = copy.deepcopy(users)
+pad_zero(users_zero.values())
