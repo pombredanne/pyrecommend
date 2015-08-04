f3dc7eaa9e67eaad74a84b9ba1307c9f3dff6da5
Daniel Passaro <daniel.passaro@regionalhelpwanted.com>
2015-08-04 15:02:50 -0400

    Make dot_product usable as a similarity function

--- pyrecommend/rec.py
+++ pyrecommend/rec.py
@@ -59,15 +59,17 @@ class DictData(object):
         return self.data.values()
 
 
-def dot_product(data_a, data_b, common_keys):
+def dot_product(data_a, data_b, all_keys=None):
     """Get the dot product between two dictionaries.
 
     Uses each key given in common_keys. I.e. each common_key is a dimension,
     and the dict value for that key is the scalar value for that dimension.
 
     """
+    if all_keys is None:
+        all_keys = data_a.item_set | data_b.item_set
     result = 0
-    for k in common_keys:
+    for k in all_keys:
         result += data_a[k] * data_b[k]
     return result
 
