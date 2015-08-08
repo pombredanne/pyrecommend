886e8b609bd8a79b2eb21e55c27f62de34024eed
Dan Passaro <danpassaro@gmail.com>
2015-08-08 12:23:38 -0400

    Take lists in similarity functions, not dicts

--- tests/test_similarity.py
+++ tests/test_similarity.py
@@ -6,16 +6,10 @@ from __future__ import (absolute_import, division, print_function,
 from pyrecommend import rec, similarity
 
 
-def make_data(*values):
-    """Make a 1-indexed DictData from a list."""
-    return rec.DictData({i+1: v for i, v in enumerate(values)},
-                        default=0)
-
-
 def test_cosine_similarity():
     """Cosine similarity calculations are correct."""
-    data_a = make_data(2, 1, 0, 2, 0, 1, 1, 1)
-    data_b = make_data(2, 1, 1, 1, 1, 0, 1, 1)
+    data_a = [2, 1, 0, 2, 0, 1, 1, 1]
+    data_b = [2, 1, 1, 1, 1, 0, 1, 1]
 
     sim_score = similarity.cosine(data_a, data_b)
 
@@ -24,8 +18,8 @@ def test_cosine_similarity():
 
 def test_dot_product():
     """Dot product calculations are correct."""
-    data_a = make_data(5, 0, 3, 2)
-    data_b = make_data(1, 3, 4, 9)
+    data_a = [5, 0, 3, 2]
+    data_b = [1, 3, 4, 9]
 
     sim_score = similarity.dot_product(data_a, data_b)
 
@@ -34,8 +28,8 @@ def test_dot_product():
 
 def test_sorensen():
     """Sorensen coefficients are correct."""
-    data_a = make_data(4, 0, 1, 3)
-    data_b = make_data(3, 1, 0, 5)
+    data_a = [4, 0, 1, 3]
+    data_b = [3, 1, 0, 5]
 
     sim_score = similarity.sorensen(data_a, data_b)
 
