681e70370a6189296682263f584857313287101c
Dan Passaro <danpassaro@gmail.com>
2015-08-08 13:31:01 -0400

    Remove probably not needed helper

--- pyrecommend/helpers.py
+++ pyrecommend/helpers.py
@@ -21,30 +21,3 @@ def make_data(score_list):
     data = {i+1: {j+1: v for j, v in enumerate(scores) if v != 0}
             for i, scores in enumerate(score_list)}
     return data
-
-
-def sim(*data, **kwargs):
-    """Shorthand for quickly calculating similarity for small datasets.
-
-    data: one or more lists of ratings. Each list represents an 'item' and each
-          number in that list is a rating by a user; users are assumed to be
-          the same between all lists.
-
-    similarity: a keyword-only argument; the similarity function to use.
-
-    Useful for playing in the shell.
-
-    >>> from .similarity import dot_product
-    >>> sim([1, 0, 5], [1, 1, 1], similarity=dot_product)
-    {(1, 2): 6}
-
-    """
-
-    # Python 2 doesn't support keyword-only arguments.
-    similarity = kwargs.pop('similarity')
-    if kwargs:
-        raise ValueError('Unused kwargs: {}'.format(kwargs.keys()))
-
-    ratings = make_data(data)
-    sim_data = rec.calculate_similarity(ratings, similarity)
-    return sim_data
