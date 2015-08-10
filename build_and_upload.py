f9a7e245b998f45eb3cc8d86c5677c331ff2e4a6
Daniel Passaro <daniel.passaro@regionalhelpwanted.com>
2015-08-10 16:22:25 -0400

    Push just the new tag, not all tags

--- build_and_upload.py
+++ build_and_upload.py
@@ -73,7 +73,7 @@ def tag_git_commit_with_version():
     tag_str = 'v{}'.format(current_version)
     print('Tagging', tag_str, 'in git')
     git('tag', tag_str)
-    git('push', '--tags')
+    git('push', 'origin', tag_str)
 
 
 if __name__ == '__main__':
